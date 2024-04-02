16cdada62a7460ab93c713de87a9d5284ed4701b
bizzan <ovouvogh@gmail.com>
2024-04-02 12:13:21 +0800

    近日有恶意利用本开源项目代码忽悠，请大家擦亮眼睛，我的联系方式是唯一的，没有其他。

--- wallet_rpc/bitcoin-rpc/src/main/java/com/spark/blockchain/rpcclient/BitcoinPaymentListener.java
+++
@@ -1,9 +0,0 @@
-package com.spark.blockchain.rpcclient;
-
-import com.spark.blockchain.rpcclient.Bitcoin.Transaction;
-
-public interface BitcoinPaymentListener {
-    void block(String var1);
-
-    void transaction(Transaction var1);
-}
